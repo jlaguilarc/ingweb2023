58ac26b63d27359b19d44e192ad0e924378b55ca
Josep Aguilar <jaguilar315@unab.edu.co>
2023-11-02 18:26:27 -0500

    AAA

---
+++ Controller/procesar.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_POST["txtNombre"];
+$correo = $_POST["txtCorreo"];
+$genero = $_POST["rdGenero"];
+
+echo "Su nombre es: ".$nombre;
+echo "Su correo es: ".$correo;
+echo "Su genero es: ".$genero;
+
+
+?>
